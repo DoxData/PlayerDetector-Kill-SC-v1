10f674a8cb4e71d6b1b31fae67faee5bf782ea41
DoxData <116974440+DoxData@users.noreply.github.com>
2025-03-28 02:55:19 +0100

    Update pero.cs

--- PlayerDetector-Kill-SC/pero.cs
+++ PlayerDetector-Kill-SC/pero.cs
@@ -1,4 +1,6 @@
 ﻿// Rename file to follow naming conventions
+// Copyright (c) 2024 DoxData. Propiedad exclusiva. 
+// Prohibido su uso/modificación sin autorización expresa.
 namespace PlayerDetector_Kill_SC
 {
     internal class Pero
